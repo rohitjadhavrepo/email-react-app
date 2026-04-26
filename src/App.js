7d16be28e224acb218dcd994eef84872487772a8
Rohit <rohitjadhav5802@gmail.com>
2026-04-26 22:16:15 +0530

    update new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         * React Based Application *
+         * React Based Application
+           Email Approval *
         </p>
         <a
           className="App-link"
