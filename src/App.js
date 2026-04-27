264a3ac40a6b7781ba15462320180253d41749ba
Rohit <rohitjadhav5802@gmail.com>
2026-04-27 15:12:37 +0530

    this this updated project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          * React Based Application
-           Email Approval *
+           Email Approval V2*
         </p>
         <a
           className="App-link"
